8bfedb4a0abed153618c9b3ef2b2ae2b1fb1e8f6
alexander.eze.ruiz-mora <alexander.eze.ruiz-mora@microfocus.com>
2020-08-03 18:31:21 -0600

    headed size changed

--- Jenkinsfile_arcsight_th_cluster.groovy
+++ Jenkinsfile_arcsight_th_cluster.groovy
@@ -1,6 +1,6 @@
 def setDescription() { 
   def item = Jenkins.instance.getItemByFullName(env.JOB_NAME) 
-  item.setDescription("<h3>This jobs installs Telegraf with the required parameters to scrape metrics from the kafka-manager pod in a given master node, it also scrapes the topic size of the th-cef, th-arcsight-avro and the th-binary_esm topics from the worker nodes</h3> \n<h3>Dashboard: <a href=\"https://15.214.145.90:8083/d/H3TCoAjWz/th-kafka-metrics-single-instance-node-metrics?orgId=5&refresh=1m\">TH Kafka Metrics (Single Instance) + Node Metrics</a></h3>") 
+  item.setDescription("<h5 style=\"color:#138D75\">This jobs installs Telegraf with the required parameters to scrape metrics from the kafka-manager pod in a given master node, it also scrapes the topic size of the th-cef, th-arcsight-avro and the th-binary_esm topics from the worker nodes.<br>Dashboard: <a href=\"https://15.214.145.90:8083/d/H3TCoAjWz/th-kafka-metrics-single-instance-node-metrics?orgId=5&refresh=1m\">TH Kafka Metrics (Single Instance) + Node Metrics</a></h5>") 
   item.save()
   }
 setDescription()
@@ -16,41 +16,41 @@ pipeline {
   parameters {
         string(
 		name: 'Master', 
-		description: '<h4>Only 1 master node ip is needed, even if the cluster has multiple masters only provide 1.</h4>'
+		description: '<h5>Only 1 master node ip is needed, even if the cluster has multiple masters only provide 1.</h5>'
 		)
 		string(
 		name: 'Workers', 
-		description: '<h4>Worker nodes ip separated by comma, e.g 15.214.x.x<span style=\"color:red\";>, </span>15.214.x.x, 15.214.x.x<span style=\"color:red\">, </span>15.214.x.x</h4>'
+		description: '<h5>Worker nodes ip separated by comma, e.g 15.214.x.x<span style=\"color:red\";>, </span>15.214.x.x, 15.214.x.x<span style=\"color:red\">, </span>15.214.x.x</h5>'
 		)
         password(
 		name: 'Host_Password', 
 		defaultValue: 'arst@dm1n', 
-		description: '<h4>Host root\'s password. The default password is <span style=\"color:red\">arst@dm1n</span>, you can change it by clicking on \"Change Password\".</h4>'
+		description: '<h5>Host root\'s password. The default password is <span style=\"color:red\">arst@dm1n</span>, you can change it by clicking on \"Change Password\".</h5>'
 		)
         string(
 		name: 'Interval', 
 		defaultValue: '10', 
-		description: '<h4>How often(in seconds) the metrics will be scrapped, default is 10 seconds.</h4>'
+		description: '<h5>How often(in seconds) the metrics will be scrapped, default is 10 seconds.</h5>'
 		)
         string(
 		name: 'InfluxDB', 
 		defaultValue: '15.214.128.179', 
-		description: '<h4>The InfluxDB server ip/hostname where the metrics will be sent. \nIf you don\'t have an InfluxDB instance you can use <span style=\"color:red\">15.214.128.179</span> with the database name <span style=\"color:red\">system_test</span></h4>'
+		description: '<h5>The InfluxDB server ip/hostname where the metrics will be sent. \nIf you don\'t have an InfluxDB instance you can use <span style=\"color:red\">15.214.128.179</span> with the database name <span style=\"color:red\">system_test</span></h5>'
 		)
         string(
 		name: 'Database', 
 		defaultValue: 'system_test', 
-		description: '<h4>The data base name where the metrics will be stored. \nIf you don\'t have an InfluxDB instance you can use <span style=\"color:red\">15.214.128.179</span> with the database name <span style=\"color:red\">system_test</span></h4>'
+		description: '<h5>The data base name where the metrics will be stored. \nIf you don\'t have an InfluxDB instance you can use <span style=\"color:red\">15.214.128.179</span> with the database name <span style=\"color:red\">system_test</span></h5>'
 		)
 		string(
 		name: 'Username', 
 		defaultValue: '', 
-		description: '<h4>The influxDB username to authenticate before sending metrics to InfluxDB, leave empty if this wasn\'t setup on your influxDB instance</h4>'
+		description: '<h5>The influxDB username to authenticate before sending metrics to InfluxDB, leave empty if this wasn\'t setup on your influxDB instance</h5>'
 		)
 		password(
 		name: 'Username_Password', 
 		defaultValue: '', 
-		description: '<h4>The influxDB username\'s password to authenticate before sending metrics to InfluxDB, leave empty if this wasn\'t setup on your influxDB instance</h4>'
+		description: '<h5>The influxDB username\'s password to authenticate before sending metrics to InfluxDB, leave empty if this wasn\'t setup on your influxDB instance</h5>'
 		)		
     }
 	
