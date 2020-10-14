fd4cb4f2a1f646e8d7ac8a43fc82fb7b38bdeaf9
alexander.eze.ruiz-mora <alexander.eze.ruiz-mora@microfocus.com>
2020-10-13 22:04:01 -0600

    get bytes insted og kb

--- files/kafka_topic_size.sh
+++ files/kafka_topic_size.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/env bash
 
 # Here I cd fisrt because after the +100 partitions du complians there are too many arguments, it's better to enter the directory and then retrieve the size of the partitions, you could do cd only once and then chain(;) the rest of the commands, it will be 1 line but it is a quite cumbersome reading through that, beside the execution time remains below 1 second so it's better to have a clear view of what we are excecuting:
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-cef-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-cef-[0-9][0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size_II="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-cef-[0-9][0-9][0-4]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size_III="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-cef-[0-9][0-9][5-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size_IV="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-arcsight-avro-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-arcsight-avro-[0-9][0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size_II="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-arcsight-avro-[0-9][0-9][0-4]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size_III="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-arcsight-avro-[0-9][0-9][5-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size_IV="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-binary_esm-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-binary_esm-[0-9][0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size_II="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-binary_esm-[0-9][0-9][0-4]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size_III="$1}'
-cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb th-binary_esm-[0-9][0-9][5-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size_IV="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-cef-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-cef-[0-9][0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size_II="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-cef-[0-9][0-9][0-4]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size_III="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-cef-[0-9][0-9][5-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-cef Partition_Size_IV="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-arcsight-avro-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-arcsight-avro-[0-9][0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size_II="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-arcsight-avro-[0-9][0-9][0-4]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size_III="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-arcsight-avro-[0-9][0-9][5-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-arcsight-avro Partition_Size_IV="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-binary_esm-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-binary_esm-[0-9][0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size_II="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-binary_esm-[0-9][0-9][0-4]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size_III="$1}'
+cd /opt/arcsight/k8s-hostpath-volume/th/kafka/ && du -sb --total th-binary_esm-[0-9][0-9][5-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=th-binary_esm Partition_Size_IV="$1}'
 
 # For used-defined topics, the name of the topic must be passed in the directory and the tag field as well, e.g
-#du -sb /opt/arcsight/k8s-hostpath-volume/th/kafka/JMETER-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=JMETER Partition_Size="$1}'
+#du -sb --total /opt/arcsight/k8s-hostpath-volume/th/kafka/JMETER-[0-9]/*.log | tail -1 | awk '{print "exec_Kafka_Topic_Size_Metrics,attribute=none,Topic=JMETER Partition_Size="$1}'
