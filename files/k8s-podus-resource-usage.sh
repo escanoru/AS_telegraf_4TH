64f9f5aff98db5d71c31c1e9e035513d9c7be629
alexander.eze.ruiz-mora <alexander.eze.ruiz-mora@microfocus.com>
2020-11-16 10:57:59 -0600

    Adding SP pod metrics

--- files/k8s-podus-resource-usage.sh
+++ files/k8s-podus-resource-usage.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Kafka and C2AV pod resources usage (Milicores request and Memory usage)
-kubectl top po -n $(kubectl get pod --all-namespaces | grep -Eio "arcsight-installer-([0–9a-z|a-z0-9]+)" | head -1) | grep -E "th-kafka-[0-9+]|th-c2av-processor-[0-9+]" | awk '{ gsub("m","",$2) ; gsub("Mi","",$3) ; print "exec_K8s_Pod_Resources_Usage,POD="$1" CPU_Mili_Cores_Request="$2",MEM_Usage="$3}'
+#kubectl top po -n $(kubectl get pod --all-namespaces | grep -Eio "arcsight-installer-([0–9a-z|a-z0-9]+)" | head -1) | grep -E "th-kafka-[0-9+]|th-c2av-processor-[0-9+]" | awk '{ gsub("m","",$2) ; gsub("Mi","",$3) ; print "exec_K8s_Pod_Resources_Usage,POD="$1" CPU_Mili_Cores_Request="$2",MEM_Usage="$3}'
 
 # Same as the above but includes the h-routing-processor-groupX-X pods
-# kubectl top po -n $(kubectl get pod --all-namespaces | grep -Eio "arcsight-installer-([0–9a-z|a-z0-9]+)" | head -1) | grep -E "th-kafka-[0-9+]|th-c2av-processor-[0-9+]|th-routing-processor-group[0-9+]-[0-9+]" | awk '{ gsub("m","",$2) ; gsub("Mi","",$3) ; print "exec_K8s_Pod_Resources_Usage,POD="$1" CPU_Mili_Cores_Request="$2",MEM_Usage="$3}'
+kubectl top po -n $(kubectl get pod --all-namespaces | grep -Eio "arcsight-installer-([0–9a-z|a-z0-9]+)" | head -1) | grep -E "th-kafka-[0-9+]|th-c2av-processor-[0-9+]|th-routing-processor-group[0-9+]-[0-9+]" | awk '{ gsub("m","",$2) ; gsub("Mi","",$3) ; print "exec_K8s_Pod_Resources_Usage,POD="$1" CPU_Mili_Cores_Request="$2",MEM_Usage="$3}'
