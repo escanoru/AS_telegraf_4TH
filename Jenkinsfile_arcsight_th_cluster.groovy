89b702e0776c416c7f47879113b8a43dcfa1e120
alexander.eze.ruiz-mora <alexander.eze.ruiz-mora@microfocus.com>
2021-07-03 20:04:27 -0600

    master

--- Jenkinsfile_arcsight_th_cluster.groovy
+++ Jenkinsfile_arcsight_th_cluster.groovy
@@ -65,7 +65,7 @@ pipeline {
 		stage('Check Ansible Inventory File') {
 			steps {
 				sh '''
-				echo -e "[all]\\n\\n[workers]" >  ${WORKSPACE}/inventory.ini | cat ${WORKSPACE}/inventory.ini
+				echo -e "[master]\\n\\n[workers]" >  ${WORKSPACE}/inventory.ini | cat ${WORKSPACE}/inventory.ini
 				echo ${Master} | sed \'s/,/\\n/g\' | while read line ; do sed -i \'/\\[master\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
 				echo ${Workers} | sed \'s/,/\\n/g\' | while read line ; do sed -i \'/\\[workers\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
 				'''
