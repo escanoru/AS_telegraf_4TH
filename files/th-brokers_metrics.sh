a44dab52e76e120e3fa17c584cf1c4fbc2274df2
alexander.eze.ruiz-mora <alexander.eze.ruiz-mora@microfocus.com>
2021-08-09 12:06:10 -0600

    path updated

--- files/th-brokers_metrics.sh
+++ files/th-brokers_metrics.sh
@@ -2,4 +2,4 @@
 
 #Rquest RSP metrics
 sleep 0.1
-curl -s --noproxy 127.0.0.1 -k -u  "admin:atlas" https://127.0.0.1:32080/cluster/broker/metric > /opt/th_kafka_brokers_metrics.json
+curl -s --noproxy 127.0.0.1 -k -u  "admin:atlas" https://127.0.0.1:32080/cluster/broker/metric > /opt/TH_SCRAPPER/th_kafka_brokers_metrics.json
--- files/th-rsp-metrics-request.sh
+++ files/th-rsp-metrics-request.sh
@@ -2,4 +2,4 @@
 
 #Rquest RSP metrics
 sleep 0.1
-curl -s --noproxy 127.0.0.1 -k -u "admin:atlas" https://127.0.0.1:32080/monitoring/streamprocessor/metrics > /opt/th_kafka_rsp_metrics.json
+curl -s --noproxy 127.0.0.1 -k -u "admin:atlas" https://127.0.0.1:32080/monitoring/streamprocessor/metrics > /opt/TH_SCRAPPER/th_kafka_rsp_metrics.json
