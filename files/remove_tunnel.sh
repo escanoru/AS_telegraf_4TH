32bff0c66e9085349b8d53fa3252e6d2f6714d9b
alexander.eze.ruiz-mora <alexander.eze.ruiz-mora@microfocus.com>
2021-08-15 14:33:33 -0600

    shell: |

--- files/remove_tunnel.sh
+++ files/remove_tunnel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for pid in $(ps -ef | grep -E "kubectl port-forward -n arcsight-" | awk '{print $2}')
-  do kill -9 $pid;
-done
-
-# for pid in $(ps -ef | grep -E "ssh -i ~/.ssh/localhost_id_rsa -o StrictHostKeyChecking=no .*" | awk '{print $2}')
+# for pid in $(ps -ef | grep -E "kubectl port-forward -n arcsight-" | awk '{print $2}')
   # do kill -9 $pid;
 # done
+
+for pid in $(ps -ef | grep -E "ssh -i ~/.ssh/localhost_id_rsa -o StrictHostKeyChecking=no .*" | awk '{print $2}')
+ do kill -9 $pid;
+done
 
 # In one line:
 # for pid in $(ps -ef | grep -E "ssh -i ~/.ssh/localhost_id_rsa -o StrictHostKeyChecking=no .*" | awk '{print $2}') ; do kill -9 $pid ; done
