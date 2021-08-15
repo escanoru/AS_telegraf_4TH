3022a4d65f6eb8fecc5c66c6c59019e654418e9a
alexander.eze.ruiz-mora <alexander.eze.ruiz-mora@microfocus.com>
2021-08-15 16:22:38 -0600

    StrictHostKeyChecking

--- files/remove_tunnel.sh
+++ files/remove_tunnel.sh
@@ -8,5 +8,5 @@ for pid in $(ps -ef | grep -E "StrictHostKeyChecking=no -o PasswordAuthenticatio
  do kill -9 $pid;
 done
 
-# In one line:
+# In one line: 
 # for pid in $(ps -ef | grep -E "ssh -i ~/.ssh/localhost_id_rsa -o StrictHostKeyChecking=no .*" | awk '{print $2}') ; do kill -9 $pid ; done
